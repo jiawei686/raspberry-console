0b6aff555ac66b4e3d061e5087bdcfadc3f2487b
Cui Jiawei <892001108@qq.com>
2018-05-01 17:11:10 +0800

    footer change

--- src/components/Dashboard/Layout/ContentFooter.vue
+++ src/components/Dashboard/Layout/ContentFooter.vue
@@ -11,7 +11,7 @@
       <div class="copyright text-center">
         &copy; Coded by
         <a href="http://cuijiawei.cn" target="_blank">Jiawei</a>.
-        Designed by <a href="https://www.creative-tim.com/?ref=pdf-vuejs" target="_blank">Creative Tim</a>.
+        Designed idea from <a href="https://www.creative-tim.com/?ref=pdf-vuejs" target="_blank">Creative Tim</a>.
       </div>
     </div>
   </footer>
